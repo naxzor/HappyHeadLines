3a40382d7186a158bcfc6bd436990238c52a61e2
NaX <naxzor1989@hotmail.com>
2025-10-03 13:12:04 +0200

    Add RegionScope migration

---
+++ ArticleDatabase/Migrations/20251003081917_AddRegionScope.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArticleDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRegionScope : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_articles_PublishedAt",
+                table: "articles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegionScope",
+                table: "articles",
+                type: "text",
+                nullable: false,
+                defaultValue: "GLOBAL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_articles_RegionScope_PublishedAt",
+                table: "articles",
+                columns: new[] { "RegionScope", "PublishedAt" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_articles_RegionScope_PublishedAt",
+                table: "articles");
+
+            migrationBuilder.DropColumn(
+                name: "RegionScope",
+                table: "articles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_articles_PublishedAt",
+                table: "articles",
+                column: "PublishedAt");
+        }
+    }
+}
---
+++ ArticleDatabase/Migrations/20251003091338_RemoveRegionScopeDefault.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArticleDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRegionScopeDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegionScope",
+                table: "articles",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldDefaultValue: "GLOBAL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegionScope",
+                table: "articles",
+                type: "text",
+                nullable: false,
+                defaultValue: "GLOBAL",
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
