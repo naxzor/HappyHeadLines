0cb19fb2474c86a418a86cd586842b6e036d8e4c
NaX <naxzor1989@hotmail.com>
2025-10-21 02:13:49 +0200

    add contracts

--- ArticleDatabase/Models/Article.cs
+++ ArticleDatabase/Models/Article.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Contracts;
 
 namespace ArticleDatabase.Models;
 
---
+++ Contracts/ArticlePublished.cs
@@ -0,0 +1,12 @@
+﻿namespace Contracts;
+
+public record ArticlePublished(
+    Guid Id,
+    string Title,
+    string Body,
+    Guid AuthorId,
+    string Language,
+    string[] Tags,
+    RegionScope RegionScope,
+    DateTime? PublishedAt
+);
